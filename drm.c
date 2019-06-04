ea8a4da66d3c518065b6af29db55dcebdf6a3330
Zhibin Li <haoyouab@gmail.com>
2019-06-04 23:45:55 +0800

    drm: add dispatcher and driver identification for DRM

--- drm.c
+++ drm.c
@@ -21,6 +21,56 @@
 #define PRINT_IOWR(nr, size, str) \
 	tprintf("DRM_IOWR(%#x, %#x) /* %s */", nr, size, str)
 
+static inline int
+drm_is_priv(const unsigned int num)
+{
+	return (_IOC_NR(num) >= DRM_COMMAND_BASE &&
+		_IOC_NR(num) < DRM_COMMAND_END);
+}
+
+static char *
+drm_get_driver_name(struct tcb *tcp)
+{
+	char path[PATH_MAX];
+	char link[PATH_MAX];
+	int ret;
+
+	if (getfdpath(tcp, tcp->u_arg[0], path, PATH_MAX - 1) < 0)
+		return NULL;
+
+	if (snprintf(link, PATH_MAX, "/sys/class/drm/%s/device/driver",
+	    basename(path)) >= (signed int)sizeof(link))
+		return NULL;
+
+	ret = readlink(link, path, PATH_MAX - 1);
+	if (ret < 0)
+		return NULL;
+
+	path[ret] = '\0';
+	return strdup(basename(path));
+}
+
+static int
+drm_is_driver(struct tcb *tcp, const char *name)
+{
+	char *priv;
+
+	/*
+	 * If no private data is allocated we are detecting the driver name for
+	 * the first time and must resolve it.
+	 */
+	if (tcp->_priv_data == NULL) {
+		priv = drm_get_driver_name(tcp);
+
+		if (priv == NULL)
+			return 0;
+
+		set_tcb_priv_data(tcp, priv, free);
+	}
+
+	return strncmp(name, get_tcb_priv_data(tcp), DRM_MAX_NAME_LEN) == 0;
+}
+
 int
 drm_decode_number(struct tcb *const tcp, const unsigned int code)
 {
